c7b5bb1210b53838f1620c8a46ab1da845c53897
Your Name <you@example.com>
2019-02-03 20:43:05 +0900

    Add

--- docs/txt/190127_htmltxt.py
+++ docs/txt/190127_htmltxt.py
@@ -8,6 +8,7 @@ header = """<!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
 body {
     font-family: monospace;
